59490346e1a465826f6ddfea952a70d3fb3349ac
Paul Keating <palisar@hotmail.co.uk>
2022-01-04 14:01:16 +0000

    Added button to return to index

---
+++ CBoardingsWebApp/Pages/Navy/Boarded.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CBoardings.WebApp.Pages
+{
+    public class BoardedModel : PageModel
+    {
+        private readonly IBoardingData _boardingData;
+        private readonly IHtmlHelper htmlHelper;
+
+        [BindProperty]
+        public Boarding Boarding { get; set; }
+        public IEnumerable<SelectListItem> CompassNS { get; set; }
+        public IEnumerable<SelectListItem> CompassEW { get; set; }
+        public BoardedModel(IBoardingData boardingData, IHtmlHelper htmlHelper)
+        {
+            _boardingData = boardingData;
+            this.htmlHelper = htmlHelper;
+            ;
+        }
+        public IActionResult OnGet()
+        {
+            CompassNS = htmlHelper.GetEnumSelectList<CompassNS>();
+            CompassEW = htmlHelper.GetEnumSelectList<CompassEW>();
+            Boarding = new Boarding();
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _boardingData.AddBoarding(Boarding);
+            return RedirectToPage("../Index");
+            
+        }
+    }
+}
