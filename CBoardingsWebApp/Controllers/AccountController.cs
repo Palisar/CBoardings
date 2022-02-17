c075b8d7e29f6cdeb43401c2bcb4b567fc11fe56
Paul Keating <palisar@hotmail.co.uk>
2022-02-17 13:12:21 +0000

    Added User Login with Auth0

---
+++ CBoardingsWebApp/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CBoardings.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task Login(string returnUrl = "/")
+        {
+            await HttpContext.ChallengeAsync("Auth0", new AuthenticationProperties() {RedirectUri = returnUrl});
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(new AuthenticationProperties()
+            {
+                RedirectUri = Url.Page("Index")
+            });
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
